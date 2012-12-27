7c7d381f30bc20d932a609d548a8c5afee3903a2
jamie <jamie@jamie-acer.(none)>
2012-12-26 19:27:24 -0500

    updated ignore settings updated core css updated index to split to separate files in includes adding jqueryui updated global header

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
 			$qrySubpagesSql_filter = $qryPage['parentPageID'];
 		}
 
-		$qrySubpagesSql = 'pageID,pageFolder,sectionFolder,groupFolder,menuText from cms_groups g join cms_sections s on g.groupid=s.groupid join cms_pages p on s.sectionid=p.sectionid where p.parentPageID=' . $qrySubpagesSql_filter;
+		$qrySubpagesSql = 'pageID,pageFolder,sectionFolder,groupFolder,menuText from cms_groups g join cms_sections s on g.groupid=s.groupid join cms_pages p on s.sectionid=p.sectionid where p.parentPageID=' . $qrySubpagesSql_filter . ' order by pageSortOrder';
 		$qryCommentsSql = 'entryName,entryEmail,entryEmailHide,entryComment,entryDtsAdd from cms_pageComments where isApproved=1 and pageID=' . $qryPage['pageID'];
 
 		$qryPhotoGallery = dbSelect($qryPhotoGallerySql);
@@ -82,169 +82,21 @@
 	if (empty($qryPage) || sizeof($qryPage) == 0){
 		echo 'Cannot find that page in the database - check your bookmark or use the menu.';
 	}elseif ($qryPage["passkey"] != "" && !isset($_COOKIE["publicAuth" . $qryPage["pageID"]])){
-		echo '<h1>This page is password protected</h1>';
-		if (isset($_GET["failed"]) && $_GET["failed"]){
-			echo '<p align="center"><strong><em>INVALID PASSWORD</em></strong></p><hr>';
-		}
-		echo '<p align="center">This page is password protected. Please enter the password below to view it.</p>';
-		echo '<form action="/password-check.php" method="post">';
-		echo '	<input type="hidden" name="pageID" value="' . $qryPage["pageID"] . '">';
-		echo '	<input type="hidden" name="returnToPath" value="/' . $qryPage["groupFolder"] . '/' . $qryPage["sectionFolder"] . '/' . $qryPage["pageFolder"] . '/">';
-		echo '	<p align="center">';
-		echo '		<input type="text" name="passkey" maxlength="16" length="16">';
-		echo '		<input type="submit" value="OK">';
-		echo '	</p>';
-		echo '</form>';
+		include('phpincludes/password.php');
 	}else{
-		// page hits by m/d/yy and browser //
-		$thisYear    = date('Y');
-		$thisMonth   = date('m');
-		$thisDay     = date('d');
-//		$thisBrowser = substr($_SERVER['HTTP_USER_AGENT'], 0, 256);
-
-		dbOpen();
-			dbInsert("
-				cms_pageHits(
-					pageID,
-					hit_month,
-					hit_day,
-					hit_year
-				)values(
-					" . $qryPage['pageID'] . ",
-					" . $thisMonth . ",
-					" . $thisDay . ",
-					" . $thisYear . "
-				)
-			");
-		dbClose();
+		include('phpincludes/addPageHit.php');
 
 		echo '<h1>' . $qryPage["pageTitle"] . '</h1>';
 
-		if ($qryPage["hasMusic"]){
-			echo '<p align="center"><embed src="/'.$qryPagePath.'/music.mp3" width="300" height="32" autostart="true" loop="false"></embed></p>';
-		}
-
-//		$pageContent = file_get_contents($qryPagePath . '/content.php');
-//		echo $pageContent;
+		include('phpincludes/music.php');
 		include($qryPagePath . '/content.php');
-
-		if (mysql_num_rows($qryPhotoGallery) > 0){
-			echo '<h2>Photo Gallery</h2>';
-
-			while ($rowPhotoGallery = mysql_fetch_assoc($qryPhotoGallery)) {
-				echo '<div class="pagePhoto" style="float:left;margin:5px;">';
-				echo '<table class="pagePhoto" border="0" cellspacing="0" cellpadding="0" style="border:1px solid #000;">';
-				echo '<tr><td align="center" style="background-color:#000;width:120px;height:120px;"><a rel="lightbox[photoGallery]" title="'.$rowPhotoGallery["caption"].'" href="/' .$qryPagePath . '/large/' . $rowPhotoGallery["imageFilename"] . '"><img src="/' . $qryPagePath . '/thumb/' . $rowPhotoGallery["imageFilename"] . '"></a></td></tr>';
-				echo '<tr><td align="center" style="background-color:#000;color:#fff;">' . $rowPhotoGallery["caption"] . '</td></tr>';
-				echo '</table>';
-				echo '</div>';
-			}
-			echo '<br style="clear:both;">';
-		}
-
-		if (mysql_num_rows($qryDownloads) > 0){
-			echo '<h2>Downloads</h2>';
-			while ($rowDownloads = mysql_fetch_assoc($qryDownloads)) {
-				echo '<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . dspIcon("download") . ' <a href="/' . $qryPagePath . '/downloads/' . $rowDownloads["filename"] . '">' . $rowDownloads["downloadText"] . '</a></p>';
-			}
-		}
-
-		if (mysql_num_rows($qrySubpages) > 0){
-			echo '<h2>Other Pages in this Section</h2>';
-			while ($rowSubpages = mysql_fetch_assoc($qrySubpages)) {
-				if ($rowSubpages["pageID"] != $qryPage["pageID"]){
-					echo '<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . dspIcon("page") . ' <a href="/' . $rowSubpages["groupFolder"] . '/' . $rowSubpages["sectionFolder"] . '/' . $rowSubpages["pageFolder"] . '/">' . $rowSubpages["menuText"] . '</a></p>';
-				}else{
-					echo '<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . dspIcon("page") . ' ' . $rowSubpages["menuText"] . '</p>';
-				}
-			}
-			if ($qryPage["parentPageID"] > 0){
-				echo '<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . dspIcon("back") . ' <a href="">Back to parent page</a></p>';
-			}
-		}
-
-		if ($qryPage["allowPageComments"]){
-			echo '<h2>Page Comments</h2>';
-			echo '<table class="pageComments">';
-			while ($rowComments = mysql_fetch_assoc($qryComments)) {
-				echo '<tr>';
-				echo '<td class="label" align="left" width="100%"><strong>' . $rowComments["entryName"];
-				if ($rowComments["entryEmailHide"] == 1){
-					echo ' (' . $rowComments["entryEmail"] . ')';
-				}
-				echo '</strong></td>';
-				echo '<td class="label" align="right" nowrap="nowrap">' . $rowComments["entryDtsAdd"] . '</td>';
-				echo '</tr>';
-				echo '<tr>';
-				echo '<td colspan="2">' . $rowComments["entryComment"] . '</td>';
-				echo '</tr>';
-				echo '<tr><td colspan="2" class="transparent">&nbsp;</td></tr>';
-			}
-			echo '</table>';
-
-			echo '<p style="text-align:center"><button onclick="jQuery(\'#cms-page-comments-form\').toggle();">Add a comment on this page</button></p>';
-			echo '<div id="cms-page-comments-form" style="display:none;">';
-			echo '<form name="addComment" method="post" action="/comment-submit.php" onsubmit="return validateFormComment();">';
-			echo '<input type="hidden" name="pageID" value="' . $qryPage["pageID"] . '">';
-			echo '<table class="formComment" align="center">';
-			echo '<tr>';
-			echo '	<td class="label">Name:</td>';
-			echo '	<td><input type="text" name="entryName" maxlength="64" style="width:320px;"></td>';
-			echo '</tr>';
-			echo '<tr>';
-			echo '	<td class="label">Email:</td>';
-			echo '	<td>';
-			echo '		<input type="text" name="entryEmail" maxlength="128" style="width:320px;"><br/>';
-			echo '		<input type="checkbox" name="entryEmailHide" value="1"> Check this box to hide your email address';
-			echo '	</td>';
-			echo '</tr>';
-			echo '<tr>';
-			echo '	<td class="label">Comment:</td>';
-			echo '	<td><textarea name="entryComment" style="width:320px;"></textarea></td>';
-			echo '</tr>';
-			echo '<tr>';
-			echo '	<td align="center" colspan="2">';
-			echo '		<a href="#" onclick="document.getElementById(\'captcha\').src = \'/securimage/securimage_show.php?\' + Math.random(); return false"><img id="captcha" src="/securimage/securimage_show.php" alt="CAPTCHA Image" /></a><br/>';
-			echo '		<em>Click the picture for a different image</em>';
-			echo '	</td>';
-			echo '</tr>';
-			echo '<tr>';
-			echo '	<td class="label" colspan="2" style="text-align:center;">Enter the orange text from above:</td>';
-			echo '</tr>';
-			echo '<tr>';
-			echo '	<td colspan="2" align="center"><input type="text" name="captcha_code" maxlength="6" /></td>';
-			echo '</tr>';
-			echo '</table>';
-			echo '<p style="text-align:center"><input type="submit" value="Submit Comment"></p>';
-			echo '</form>';
-			echo '</div>';
-
-			echo '';
-		}
+		include('phpincludes/photoGallery.php');
+		include('phpincludes/downloads.php');
+		include('phpincludes/subpages.php');
+		include('phpincludes/comments.php');
 	}
 
 	require "phpincludes/layout.footer.inc.php";
 
-
-
-	// ********************************************************************************
-	// ********************************************************************************
-	// local debugging helper
-	// ********************************************************************************
-	// ********************************************************************************
-	if (!strpos($_SERVER['SERVER_NAME'], '.local') == false){// PRODUCTION
-		require "custom/settings.db.inc.php";
-
-		echo '<br/><br/><br/>';
-		echo '<fieldset>';
-		echo '	<legend>Settings Included:</legend>';
-		echo '	$application_dbserver = ' . $application_dbserver . '<br>';
-		echo '	$application_dbname = ' . $application_dbname . '<br>';
-		echo '	$application_dbuser = ' . $application_dbuser . '<br>';
-		echo '	$application_dbpass = ' . $application_dbpass . '<br>';
-		echo '</fieldset>';
-//		echo '<a href="/development/set-mobile-true.php">Set Mobile TRUE</a> | <a href="/development/set-mobile-false.php">Set Mobile FALSE</a><br>';
-		echo '<br/>';
-		echo 'LOCAL DEBUG >> Including page: /' . $qryPagePath . '/content.php<hr>';
-	}
+	include('phpincludes/debug.php');
 ?>
--- phpincludes/layout.header.inc.php
+++ phpincludes/layout.header.inc.php
@@ -1,27 +1,55 @@
 <!doctype html>
-<html>
+ 
+<html lang="en">
 <head>
+    <meta charset="utf-8" />
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 	<title><?php echo $siteTitle ?></title>
-	<!-- jQuery library -->
-	<script type="text/javascript" src="/jscripts/jquery-1.8.3.js"></script>
-	<!-- jQueryUI library -->
-	<link rel="stylesheet" href="/css/jquery-ui-1.9.2.custom.css">
-	<script type="text/javascript" src="/jscripts/jquery-ui-1.9.2.custom.min.js"></script>
-	<script type="text/javascript" src="/jscripts/jquery.tools.min.js"></script>
-	<!-- jQuery no-conflict -->
-	<script type="text/javascript">
-		jQuery.noConflict();
-	</script>
-
-	<!-- photo gallery needs these -->
-	<script type="text/javascript" src="/jscripts/prototype.js"></script>
-	<script type="text/javascript" src="/jscripts/scriptaculous.js?load=effects"></script>
-	<script type="text/javascript" src="/jscripts/lightbox.js"></script>
-	<link rel="stylesheet" href="/css/lightbox.css" type="text/css" media="screen" />
-
 	<!-- core css/jscripts for all sites -->
 	<script type="text/javascript" src="/jscripts/core.js"></script>
 	<link rel="stylesheet" href="/css/core.css">
+
+	<!-- jQuery library -->
+	<?php
+		if (!strpos($_SERVER['SERVER_NAME'], '.local') == true){// PRODUCTION
+			// CDN jquery, for production
+			echo '<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" />';
+			echo '<script src="http://code.jquery.com/jquery-1.8.3.js"></script>';
+			echo '<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>';
+		}else{// LOCAL
+			echo '<link rel="stylesheet" href="/css/jquery-ui-1.9.2.custom.css">';
+			echo '<script type="text/javascript" src="/jscripts/jquery-1.8.3.js"></script>';
+			echo '<script type="text/javascript" src="/jscripts/jquery-ui-1.9.2.custom.min.js"></script>';
+		}
+	?>
+
+	<!-- fancybox - jQuery plugin - for inline image zooming -->
+	<script type="text/javascript" src="/jscripts//jquery.fancybox-1.3.4.js"></script>
+	<link rel="stylesheet" type="text/css" href="/css/jquery.fancybox-1.3.4.css" media="screen" /></head>
+
+	<script type="text/javascript">
+		$(function(){
+			$("a.armoryThumbs").fancybox();
+
+			$("a.menu-group").tooltip({
+				position: {
+					my: "left top-1",
+					at: "right+1 top"
+				}
+			});
+		});
+	</script>
+	<style>
+		.ui-tooltip {
+			background: #eee;
+			border: 1px solid #000;
+			padding: 4px;
+			color: #000;
+			border-radius: 5px;
+			font: bold 12px Arial;
+			box-shadow: 3px 3px 7px black;
+		}
+    </style>
 
 	<!-- site specific -->
 	<?php
@@ -29,21 +57,21 @@
 			echo '<link rel="stylesheet" href="/custom/css/style.css">';
 		}
 	?>
-</head>
-
 <body>
 
-<div id="fb-root"></div>
-<script>
-	(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-</script>
-
 <?php
+	if (!strpos($_SERVER['SERVER_NAME'], '.local') == true){// PRODUCTION
+		echo '<div id="fb-root"></div>';
+		echo '<script>';
+		echo '	(function(d, s, id) {';
+		echo '		var js, fjs = d.getElementsByTagName(s)[0];';
+		echo '		if (d.getElementById(id)) return;';
+		echo '		js = d.createElement(s); js.id = id;';
+		echo '		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";';
+		echo '		fjs.parentNode.insertBefore(js, fjs);';
+		echo "	}(document, 'script', 'facebook-jssdk'));";
+		echo '</script>';
+	}
+
 	require $_SERVER["DOCUMENT_ROOT"] . "/custom/layout.header.inc.php";
 ?>
